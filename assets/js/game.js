b22ab0913f25248422c39c2926dcf491f997dc87
escowin <edwin@escowinart.com>
2022-03-22 22:09:40 -0500

    3.1.3 | pseudocode setup

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,28 @@
 // redux | module 03: javascript, robot gladiators
+
+// 1. prompt player for robot name
+
+// 2. starting stats (player):
+  // 100 HP
+  // 10 AP
+  // 10 money points
+
+// 3. starting stats (cpu, roborto):
+  // 50 hp
+  // 12 ap
+
+// 4. game displays 'welcome to robot gladiators'
+
+// 5. game prompts player to choose either fight or skip round
+
+// 6. if player skips:
+  // -10 money points
+  // game ends
+
+// 7. if player fights
+  // player attacks cpu: player ap - cpu hp
+  // game displays cpu's remaining hp
+  // cpu attacks player: cpu ap - player hp
+  // game displays user's remaining hp
+
+// 8. game ends.
