f841b1af3386b8245830d4c9597b569655bf8385
escowin <edwin@escowinart.com>
2022-07-02 16:47:05 -0500

    3.1.5 | js basics

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,3 +1,15 @@
 // redux
-// bonus.build out .html via js
-console.log('this is where i would have done a window.alert but i dont like the pop-up');
+  // bonus.build out .html via js
+
+
+// user input
+// var playerName = window.prompt("what is your bot's name?");
+var playerName = "bot";
+console.log("player name is " + playerName);
+
+function fight() {
+  console.log("this is the fight(), runs after the window.prompt. this message is a string.");
+};
+
+
+// fight();
