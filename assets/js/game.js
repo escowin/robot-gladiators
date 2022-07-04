cab4d56bff84b59eafbf286cba0903a0ef319ab7
escowin <edwin@escowinart.com>
2022-07-04 09:07:49 -0500

    3.2.7 | if(... break;) inside while() loop

--- assets/js/game.js
+++ assets/js/game.js
@@ -3,13 +3,13 @@
 // user input
 // var playerName = window.prompt("enter bot name");
 var playerName = "Player";
-var playerHealth = 111;
-var playerAttack = 50;
+var playerHealth = 50;
+var playerAttack = 10;
 var playerMoney = 10;
 
 var enemyNames = ["Roborto", "Android Amy", "Robo Trumble"];
-var enemyHealth = 20;
-var enemyAttack = 20;
+var enemyHealth = 50;
+var enemyAttack = 10;
 
 // var wash = function(soapType) {
 //   console.log("I wash with " + soapType);
@@ -25,54 +25,55 @@ var enemyAttack = 20;
 console.log("====== welcome =======");
 
 var fight = function (enemyName) {  
-  // var promptFight = window.prompt("SKIP or FIGHT?");
-  var promptFight = "fight";
 
-  console.log("[ " + playerName + " chooses to " + promptFight + "! ]");
-  // player chooses to FIGHT or SKIP. invalid input also adressed.
-  if (promptFight === "fight" || promptFight === "FIGHT") {
-    // player attacks
+  // repeat while current enemy is alive
+  while(playerHealth > 0 && enemyHealth > 0) {
+    // choice.fight or skip?
+    var promptFight = window.prompt("SKIP or FIGHT?");
+    // var promptFight = "skip";
+    console.log("[  " + playerName + " chooses to " + promptFight + "  ]");
+    
+    if (promptFight === "skip" || promptFight === "SKIP") {
+
+      var confirmSkip = window.confirm("skip?");
+
+      // if yes.subtract money, leave fight
+      if (confirmSkip) {
+        console.log("[ " + playerName + " skips round. ]")
+        playerMoney = playerMoney - 10;
+        console.log("[ " + playerName + " has " + playerMoney + "g remaining. ]");
+        break;
+      }
+    }
+
+    // logic.fight (default action)
+    // logic.player attacks
     enemyHealth = enemyHealth - playerAttack;
-    console.log("====== FIGHT! =======");
-    console.log(playerName + " attacks " + enemyName + ". " + enemyName + " down to " + enemyHealth + "hp.");
+    console.log(playerName + " attacks " + enemyName + " w/ " + playerAttack + "ap.");
 
-    // checks enemy health
+    // logic.check enemy health
     if (enemyHealth <= 0) {
-      console.log(enemyName + " has died bc he has " + enemyHealth + "hp.");
+      console.log(enemyName + " is dead (" + enemyHealth + "hp).");
+      break;
     } else {
-      console.log(enemyName + " not dead. " + enemyHealth + "hp remaining.");
+      console.log(enemyName + " has " + enemyHealth + "hp remaining.");
     }
-
-    // enemy attacks
+    
     playerHealth = playerHealth - enemyAttack;
-    console.log(enemyName + " attacks " + playerName + ". " + playerName + " down to " + playerHealth + "hp.");
+    console.log(enemyName + " attacks " + playerName + " w/ " + enemyAttack + "ap.");
 
-    // checks player health
+    // logic.check player health
     if (playerHealth <= 0) {
-      console.log(playerName + " has died bc he has " + playerHealth + "hp.");
+      console.log("[  " + playerName + " is dead (" + playerHealth + "hp). game over.  ]");
+      break;
     } else {
-      console.log(playerName + " not dead. " + playerHealth + "hp remaining.");
+      console.log(playerName + " has " + playerHealth + "hp remaining.");
     }
-
-  // player chooses skip
-  } else if (promptFight === "skip" || promptFight === "SKIP") {
-
-    var confirmSkip = window.confirm("skip?");
-
-    // if yes, leave fight
-    if (confirmSkip) {
-      console.log("[ " + playerName + " skips round. ]")
-      playerMoney = playerMoney - 2;
-      console.log("[ " + playerName + " has " + playerMoney + "g remaining. ]");
-    } else {
-      fight();
-    }
-
-  } else {
-    console.log("invalid input. try again.");
-  }
-};
+  } //  end.while() loop
+}; // end.fight()
 
 for(var i = 0; i < enemyNames.length; i++) {
-  fight(enemyNames[i]);
+  var pickedEnemyName = enemyNames[i];
+  enemyHealth = 50;
+  fight(pickedEnemyName);
 }
